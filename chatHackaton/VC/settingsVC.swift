57ad2df807684c847ffd952cefa335d538b71f96
Taangh <30960806+Taangh@users.noreply.github.com>
2018-03-24 15:16:15 +0100

    settings layout

--- chatHackaton/VC/settingsVC.swift
+++ chatHackaton/VC/settingsVC.swift
@@ -12,22 +12,14 @@ class settingsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return UITableViewCell()
     }
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
     func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
