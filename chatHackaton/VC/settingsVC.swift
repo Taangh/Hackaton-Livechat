34ca675fcce0b8387960dba4e3b098e525a0ada7
Justyna Jakubiak <justyna.jakubiak@zaven.pl>
2018-03-24 15:38:30 +0100

    Add view

--- chatHackaton/VC/settingsVC.swift
+++ chatHackaton/VC/settingsVC.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class settingsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class settingsVC: UIViewController, UITableViewDelegate, UITableViewDataSource, OXPatternLockDelegate {
+    func didPatternInput(patterLock: OXPatternLock, track: [Int]) {
+        
+    }
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var addButton: UIButton!
+    @IBAction func addButton(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var dotsView: UIView!
+    var locker = OXPatternLock()
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return UITableViewCell()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpDotsView()
+    }
+    
+    func setUpDotsView() {
+        locker = OXPatternLock(frame: CGRect(x: 0, y: 20, width: self.view.bounds.width, height: 260))
+        locker.delegate = self
+        locker.backgroundColor = .white
+        locker.dot = UIImage(named: "dot.png")
+        locker.dotSelected = UIImage(named: "dot-selected.png")
+        locker.trackLineThickness = CGFloat(3)
+        locker.lockSize = 3
+        locker.trackLineColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+        view.addSubview(locker)
     }
 
     func numberOfSections(in tableView: UITableView) -> Int {
